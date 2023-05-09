f0f706e18bf2a1708616b8804bfad32dd8525301
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-05-09 21:46:07 +0000

    added style

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -32,15 +32,17 @@ const Home = () => {
             <div className='container'>
 				<div className='row'>
 					<div className='col-12 col-md-7'>
-						<h2>TODOS</h2>
-						<form onKeyDown={handleAddTask} onSubmit={handleSubmit}>
-							<input value={task} type='text' placeholder='ingrese tarea' className='form-control' name='task' onChange={handleChange}/>
-						</form>
-						{
-							allTasks.map((item, index) => {
-								return (<p onClick={() =>handleDeleteTask(index)} key={index}>{item} </p>)
-							})
-						}
+						<h1>hola</h1>
+			            <div className='contenedor'>
+							<form onKeyDown={handleAddTask} onSubmit={handleSubmit}>
+						        <input value={task} type='text' placeholder='What needs to be done?' className='input form-control' name='task' onChange={handleChange}/>
+					        </form>
+							{
+							    allTasks.map((item, index) => {
+							    return (<p onClick={() =>handleDeleteTask(index)} key={index}>{item} </p>)
+							    })
+					        }
+				        </div>
 					</div>
 				</div>
 			</div>
