624437b50b4f4e978e8b7f66b48ca71e5af44c9e
Victor Pantin <125493189+tuquepantin@users.noreply.github.com>
2023-05-09 22:35:24 +0000

    added more styles

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -32,16 +32,22 @@ const Home = () => {
             <div className='container'>
 				<div className='row'>
 					<div className='col-12 col-md-7'>
-						<h1>hola</h1>
+						<h1>Todos</h1>
 			            <div className='contenedor'>
 							<form onKeyDown={handleAddTask} onSubmit={handleSubmit}>
-						        <input value={task} type='text' placeholder='What needs to be done?' className='input form-control' name='task' onChange={handleChange}/>
+						        <input value={task} 
+								       type='text' 
+									   placeholder='What needs to be done?' 
+									   className='input form-control' 
+									   name='task' 
+									   onChange={handleChange}/>
 					        </form>
 							{
 							    allTasks.map((item, index) => {
 							    return (<p onClick={() =>handleDeleteTask(index)} key={index}>{item} </p>)
 							    })
 					        }
+							<div className='task'>{allTasks.length} item left</div>
 				        </div>
 					</div>
 				</div>
